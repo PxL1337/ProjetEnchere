e58dd3e0a4bc44b0d0c0924e57655da526f393e0
Guerric Cochelin <cochelin.guerric@hotmail.com>
2023-06-01 16:40:14 +0200

    BLL Wip

--- src/main/java/fr/eni/projetenchere/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projetenchere/bll/UtilisateurManager.java
@@ -1,17 +1,142 @@
 package fr.eni.projetenchere.bll;
 
-public class UtilisateurManager {
+import java.util.List;
 
-	private static UtilisateurManager instance;
+import fr.eni.projetenchere.bo.User;
+import fr.eni.projetenchere.bo.Utilisateur;
+import fr.eni.projetenchere.dal.jdbc.UserDAOJdbcImplementation;
+
+public class UserManager {
 	
-	public static UtilisateurManager getInstance()
+	//SINGLETON
+
+	private static UserManager instance;
+	
+	public static UserManager getInstance()
 	{
 		if (instance == null) {
-			instance = new UtilisateurManager();
+			instance = new UserManager();
 		}
 		
 		return instance;
 	}
+
+	private UserDAOJdbcImplementation userDAOJdbc;	
 	
-	private UtilisateurManager() {}
+	private UserManager() {
+		userDAOJdbc = new UserDAOJdbcImplementation();}
+	
+	
+
+	
+	
+	
+	public void insertUser(User user){
+	    userDAOJdbc.insert(user);	
+	}
+	
+	public void updateUser(User user){
+	    userDAOJdbc.update(user);	
+	}
+	
+	public void deleteUser(User user){
+	    userDAOJdbc.delete(user);	
+	}
+	
+	public void updateUserCredit(User user, int newValue) {
+		userDAOJdbc.updateCredit(user, newValue);
+	}
+	
+	public User selectUserByID(int ID) {
+		return userDAOJdbc.selectByID(ID);
+	}
+	
+	public List<User> selectAllUser() {
+		return userDAOJdbc.selectAll();
+	}
+	
+	public User selectUserByPseudoOuEmail(String pseudoOuEmail) throws SQLException {
+        return userDAOJdbc.selectByPseudoOuEmail(pseudoOuEmail);
+    }
+	
+	public User selectUserByPseudo(String comparedPseudo){
+		return userDAOJdbc.selectByPseudo(comparedPseudo);
+	}
+	
+	public User selectUserByEmail(String comparedEmail){
+		return userDAOJdbc.selectByEmail(comparedEmail);
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
+//noUser	
+//Pseudo
+	
+	public boolean checkPseudoAvailability(String pseudo) {			
+	    return userDAOJdbc.doesThisPseudoAlreadyExists(pseudo);
+	}
+
+	
+//nom
+	
+	public static boolean validateNom(String nom) {
+		String nomPattern = "^[a-zA-Z]+$";
+		return nom.matches(nomPattern);
+		}
+//prenom
+	
+	public static boolean validatePrenom(String prenom) {
+		String prenomPattern = "^[a-zA-Z]+$";
+		return prenom.matches(prenomPattern);
+		}
+	
+//email
+
+	public boolean checkEmailAvailability(String email) {		
+		return userDAOJdbc.doesThisEmailAlreadyExists(email);
+	}
+
+//telephone
+	
+	public static boolean validatePhoneNumber(String phoneNumber) {
+		String phoneNumberPattern = "^\\+?[1-9]\\d{1,14}$";
+		return phoneNumber.matches(phoneNumberPattern);
+	}
+//rue
+//ville
+//codePostal
+	
+	public static boolean validateCodePostal(String codePostal) {
+		String postalCodePattern = "\\d{5}";
+		return codePostal.matches(postalCodePattern);
+	}
+
+//motDePasse
+	
+	public static boolean validateMotDePasse(String motDePasse) {
+		return motDePasse.length() >=6 && motDePasse.matches(".*[A-Z].*");		
+	}
+
+	
+//credit
+//administrateur
+	
+	
 }
+			
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
