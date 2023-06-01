6bdcd897a573c08bd051c8e8bba0a2e9b24a8c71
Nabil SI-SMAIL <nabilsys8@gmail.com>
2023-06-01 11:57:20 +0200

    constructeur avec mdp

--- src/main/java/fr/eni/projetenchere/bo/Utilisateur.java
+++ src/main/java/fr/eni/projetenchere/bo/Utilisateur.java
@@ -90,6 +90,20 @@ public class Utilisateur {
 
 
 
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse) {
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+	}
+
 	//---------------------------------------METHODE/FUNCTION ZONE---------------------------------------//
     public void addEnchereToList(Enchere enchere) {
 		listeEncheresUtilisateur.add(enchere);
