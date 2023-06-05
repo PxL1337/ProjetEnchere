2eac583ddafdd77c35a40d1377abf38cd5dd4806
Nabil SI-SMAIL <nabilsys8@gmail.com>
2023-06-05 11:29:33 +0200

    query view VueArticles

---
+++ src/main/webapp/WEB-INF/config/VueArticles.sql
@@ -0,0 +1,17 @@
+CREATE VIEW VueArticles AS
+SELECT 
+    AV.no_article,
+    AV.nom_article,
+    AV.description,
+    AV.date_debut_encheres,
+    AV.date_fin_encheres,
+    AV.prix_initial,
+    AV.prix_vente,
+    U.pseudo AS pseudo_utilisateur,
+    U.email AS email_utilisateur,
+    C.libelle AS libelle_categorie
+FROM
+    ARTICLES_VENDUS AV
+    INNER JOIN UTILISATEURS U ON AV.no_utilisateur = U.no_utilisateur
+    INNER JOIN CATEGORIES C ON AV.no_categorie = C.no_categorie;
+
